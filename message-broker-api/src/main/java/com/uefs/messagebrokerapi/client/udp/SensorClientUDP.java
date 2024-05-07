8ce3d42e75191a52051f19586b7b0a6d59a0cc1d
Joanderson90 <vcvermaisnaoobserva@gmail.com>
2024-05-07 00:29:01 -0300

    Add time out to server udp

--- message-broker-api/src/main/java/com/uefs/messagebrokerapi/client/udp/SensorClientUDP.java
+++ message-broker-api/src/main/java/com/uefs/messagebrokerapi/client/udp/SensorClientUDP.java
@@ -4,12 +4,14 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 
 public class SensorClientUDP extends Thread {
     private DatagramSocket socket;
     private InetAddress address;
     private int port;
     private byte[] buf;
+    private final int TIME_OUT_IN_MILLISECONDS = 30000;
 
     public SensorClientUDP(String ipServer, int portServer) {
         try {
@@ -28,9 +30,13 @@ public class SensorClientUDP extends Thread {
             buf = new byte[1024];
             buf = msg.getBytes();
             packet = new DatagramPacket(buf, buf.length, address, port);
+
             socket.send(packet);
+            socket.setSoTimeout(TIME_OUT_IN_MILLISECONDS);
+
             packet = new DatagramPacket(buf, buf.length);
             System.out.println("Wating data...");
+
             socket.receive(packet);
         } catch (IOException e) {
             e.printStackTrace();
